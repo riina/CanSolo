199e18e3d1641d280244490973635e943de69ca1
riina <54872398+riina@users.noreply.github.com>
2022-02-12 14:47:58 -0800

    fix ordering

--- tsconcat/Program.cs
+++ tsconcat/Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text.RegularExpressions;
+
+Regex re = new(@"(\d+)$");
 AR.Require("dir", "outfile").KeyDo((r, _) => {
     string dir = r["dir"], outfile = r["outfile"];
-    List<string> files = Directory.GetFiles(dir, "*.ts").OrderBy(v => v).ToList();
+    List<string> files = Directory.GetFiles(dir, "*.ts").OrderBy(v =>
+    {
+        Match m = re.Match(v);
+        if (!m.Success) throw new InvalidDataException($"unexpected filename format {v}");
+        return m.Groups[1].Value;
+    }).ToList();
     foreach (string file in files)
         Console.WriteLine(file);
     Wait.Enter();
--- tsgaps/Program.cs
+++ tsgaps/Program.cs
@@ -1,23 +1,28 @@
 ﻿using System.Text.RegularExpressions;
 
 Regex re = new(@"(\d+)$");
-AR.Require("dir").KeyDo((r, _) => {
+AR.Require("dir").KeyDo((r, _) =>
+{
     string dir = r["dir"];
     List<string> files = Directory.GetFiles(dir, "*.ts").OrderBy(v => v).ToList();
     if (files.Count == 0) return;
     List<long> numbers = files
         .Select(Path.GetFileNameWithoutExtension)
-        .Select(v => {
+        .Select(v =>
+        {
             Match m = re.Match(v!);
             if (!m.Success) throw new InvalidDataException($"unexpected filename format {v}");
             return m.Groups[1].Value;
         })
         .Select(long.Parse)
+        .OrderBy(v => v)
         .ToList();
     int gaps = 0;
-    for (int i = 1; i < numbers.Count; i++) {
+    for (int i = 1; i < numbers.Count; i++)
+    {
         long p = numbers[i - 1], c = numbers[i];
-        if (p + 1 != c) {
+        if (p + 1 != c)
+        {
             Console.WriteLine($"Gap between {p} and {c}");
             gaps++;
         }
